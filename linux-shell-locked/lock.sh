8a357fa81803fe17f23a98830edf30810b0561ee
charlie <cenhuqing@gmail.com>
2014-09-23 12:41:33 +0800

    Signed-off-by: charlie <cenhuqing@gmail.com>

---
+++ linux-shell-locked/lock.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+LOCKFILE=$(basename $0)_lock
+echo $LOCKFILE
+if [ -f $LOCKFILE ]
+then
+        MYPID=$(cat $LOCKFILE)
+        ps -p $MYPID &> /dev/null
+        [ $? -eq 0 ] && "already running" && exit 5
+fi
+echo $$ > $LOCKFILE
+read
+echo hello
+rm -rf $LOCKFILE
